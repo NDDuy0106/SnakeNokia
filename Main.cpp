5deb28c546bc2873402b687f8d06afe6db426378
Admin <Admin@DESKTOP-L935296>
2025-02-23 17:57:48 +0700

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,74 @@
+﻿#include<iostream>
+#include"MyLibrary.h"
+#define max 100
+
+using namespace std;
+
+int toa_do_x[max] = { 0 };
+int toa_do_y[max] = { 0 };
+int dot_ran = 4;   //Số đốt của rắn lúc đầu
+
+void play();
+void ve_tuong();
+void khoi_tao_ran();
+void ve_ran();
+//===============================
+void main()
+{
+	cout << "Press Enter to Exit ! \n";
+	play();
+	//Xóa những dòng code thừa:
+	cin.get();
+	system("cls");
+}
+
+void play()
+{
+	doi_mau(12);
+	ve_tuong();
+	khoi_tao_ran();
+	ve_ran();
+}
+
+void ve_tuong() //vẽ tường trong game
+{
+	for (int i = 10; i <= 95; i++)
+	{
+		gotoXY(i, 1);
+		cout << char(196);
+		gotoXY(i, 25);
+		cout << char(196);
+	}
+	for (int y = 1; y <= 25; y++)
+	{
+		gotoXY(10, y);
+		cout << char(179);
+		gotoXY(95, y);
+		cout << char(179);
+	}
+}
+
+void khoi_tao_ran()
+{
+	//Chọn tọa độ cho đầu rắn
+	int x_ban_dau = 50;     
+	int y_ban_dau = 13;
+	//Tạo mấy cái đốt sau
+	for (int i = 0; i < dot_ran; i++)
+	{
+		toa_do_x[i] = x_ban_dau--;
+		toa_do_y[i] = y_ban_dau;
+	}
+}
+
+void ve_ran()
+{
+	for (int i = 0; i < dot_ran; i++)
+	{
+		gotoXY(toa_do_x[i], toa_do_y[i]);
+		if (i == 0)
+			cout << "0";
+		else
+			cout << "o";
+	}
+}
---
+++ MyLibrary.h
@@ -0,0 +1,15 @@
+﻿#pragma once //Giup khong bi xung dot thu vien
+#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+
+void doi_mau(int color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void gotoXY(int x, int y)
+{
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { SHORT(x), SHORT(y) });
+}
